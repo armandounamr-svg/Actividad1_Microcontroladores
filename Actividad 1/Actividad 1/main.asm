04e6c43a510d6100dc4bcec82b9f7184f34b28bc
jflh0302 <jflh0302@gmail.com>
2026-03-06 22:04:45 -0600

    Add files via upload

---
+++ Actividad 1/Actividad 1/main.asm
@@ -0,0 +1,64 @@
+;
+; Actividad 1.asm
+;
+; Created: 06/03/2026 09:32:30 p. m.
+; Author : jflh0
+;
+
+;================================================
+; Generar 100 números pseudoaleatorios
+; ATmega328P - AVR Assembly
+; Microchip Studio
+
+;========================================
+
+
+.include "m328pdef.inc"
+
+;------------- REGISTROS ----------------
+.def rand = r16
+.def count = r17
+
+;------------- MEMORIA SRAM -------------
+.dseg
+table_of_unsorted_numbers:
+.byte 100
+
+;------------- CODIGO -------------------
+.cseg
+.org 0x0000
+rjmp RESET
+
+
+RESET:
+
+; inicializar puntero Z
+ldi r30, low(table_of_unsorted_numbers)
+ldi r31, high(table_of_unsorted_numbers)
+
+; contador de 100 numeros
+ldi count, 100
+
+; semilla inicial
+ldi rand, 37
+
+GENERATE:
+
+; algoritmo simple pseudoaleatorio
+; rand = rand*5 + 1
+
+mov r18, rand
+lsl rand
+lsl rand
+add rand, r18
+inc rand
+
+; guardar en memoria
+st Z+, rand
+
+; disminuir contador
+dec count
+brne GENERATE
+
+END:
+rjmp END
