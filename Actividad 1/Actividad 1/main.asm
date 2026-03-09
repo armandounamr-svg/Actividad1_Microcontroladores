5832068f5c41f8562f2b5feb7ee5ce5af5480567
armandounamr-svg <armandounamr@gmail.com>
2026-03-09 00:03:04 -0600

    Optimizacion y reduccion de la primera parte
    Se agregaron buenas practicas de programacion, (iniciar el satckpointer, utilizar ZL Y ZH, etc, se optimizo y comento el primer programa (Generar 100 numeros aleatorios)

--- Actividad 1/Actividad 1/main.asm
+++ Actividad 1/Actividad 1/main.asm
@@ -1,26 +1,17 @@
-;
 ; Actividad 1.asm
-;
-; Created: 06/03/2026 09:32:30 p. m.
-; Author : jflh0
-;
-
-;================================================
-; Generar 100 números pseudoaleatorios
-; ATmega328P - AVR Assembly
-; Microchip Studio
-
-;========================================
-
+; Generar 100 números pseudoaleatorios (LCG)
+; ATmega328P
 
 .include "m328pdef.inc"
 
 ;------------- REGISTROS ----------------
-.def rand = r16
+.def rand  = r16
 .def count = r17
+.def temp  = r18
 
 ;------------- MEMORIA SRAM -------------
 .dseg
+.org SRAM_START ; Aseguramos que empiece en el inicio de la RAM de datos
 table_of_unsorted_numbers:
 .byte 100
 
@@ -29,36 +20,35 @@ table_of_unsorted_numbers:
 .org 0x0000
 rjmp RESET
 
-
 RESET:
+    ; 1. Inicializar Puntero de Pila (Stack Pointer)
+    ldi temp, low(RAMEND)
+    out SPL, temp
+    ldi temp, high(RAMEND)
+    out SPH, temp
 
-; inicializar puntero Z
-ldi r30, low(table_of_unsorted_numbers)
-ldi r31, high(table_of_unsorted_numbers)
+    ; 2. Inicializar puntero Z para apuntar a la SRAM
+    ldi ZL, low(table_of_unsorted_numbers)
+    ldi ZH, high(table_of_unsorted_numbers)
 
-; contador de 100 numeros
-ldi count, 100
-;ojjojo
-; semilla inicial
-ldi rand, 37
+    ; 3. Configuración inicial
+    ldi count, 100
+    ldi rand, 37      ; Semilla inicial
 
 GENERATE:
+    ;--- Algoritmo: rand = (rand * 5) + 1 ---
+    mov temp, rand    ; Copia el valor actual
+    lsl rand          ; rand = rand * 2
+    lsl rand          ; rand = rand * 4
+    add rand, temp    ; rand = (rand * 4) + original = * 5
+    inc rand          ; rand = (rand * 5) + 1
 
-; algoritmo simple pseudoaleatorio
-; rand = rand*5 + 1
+    ;--- Guardar en memoria ---
+    st Z+, rand       ; Guarda el resultado y avanza el puntero Z
 
-mov r18, rand
-lsl rand
-lsl rand
-add rand, r18
-inc rand
-
-; guardar en memoria
-st Z+, rand
-
-; disminuir contador
-dec count
-brne GENERATE
+    ;--- Control de bucle ---
+    dec count
+    brne GENERATE
 
 END:
-rjmp END
+    rjmp END          ; Fin del programa
